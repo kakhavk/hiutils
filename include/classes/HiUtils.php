406d4f5ba8bd4c7fe4c775e4aed38e0c3326f4b2
kakhaber <kakhavk@gmail.com>
2016-01-13 14:34:26 +0400

    latToUtf8 changed to lat2GeoUtf8

--- include/classes/HiUtils.php
+++ include/classes/HiUtils.php
@@ -195,7 +195,7 @@ class HiUtils{
         return $ret;
     }
     
-    function latToUtf8($str, $setMbInternalEncoding=true){
+    function lat2GeoUtf8($str, $setMbInternalEncoding=true){
 		if($setMbInternalEncoding==true) mb_internal_encoding("UTF-8");
 		$latin_to_utf8=array('a'=>'ა','b'=>'ბ','g'=>'გ','d'=>'დ','e'=>'ე','v'=>'ვ','z'=>'ზ','T'=>'თ','i'=>'ი','k'=>'კ','l'=>'ლ','m'=>'მ','n'=>'ნ','o'=>'ო','p'=>'პ','J'=>'ჟ','r'=>'რ','s'=>'ს','t'=>'ტ','u'=>'უ','f'=>'ფ','q'=>'ქ','R'=>'ღ','y'=>'ყ','S'=>'შ','C'=>'ჩ','c'=>'ც','Z'=>'ძ','w'=>'წ','W'=>'ჭ','x'=>'ხ','j'=>'ჯ','h'=>'ჰ');
 		
