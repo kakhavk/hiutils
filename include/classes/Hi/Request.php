b78218ba578e0e1f2d461be9aa2b59948d9ef0a9
Gogs <gogs@fake.local>
2018-01-13 00:02:45 +0400

    changes

--- include/classes/Hi/Request.php
+++ include/classes/Hi/Request.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * Created by Kakhaber Kashmadze
-@version 0.3
+@version 0.4
 @license MIT
 */
 
@@ -10,100 +10,102 @@ namespace Hi;
 
 class Request{
 
-	private static $request=array();
-	
-	public static function setRequest($request){
-		if(!empty($request) && is_array($request)){
-			self::$request=self::parse($request);
-		}else{
-			self::$request=array();
-		}
-	}
-	
-	public static function getRequest(){
-		return self::$request;
-	}
-	
-	public static function query($name, $params=array()){
-		$item=null;
-		$allowZero=false;
-		if(isset($params['allowzero'])){
-			$allowZero=$params['allowzero'];
-		}
-		if(isset(self::$request[$name])){
-			$item=trim(self::$request[$name]);
-			
-			if($allowZero===false){
-				if($item=="0"){
-					return null;
-				}
-			}
-			return trim(self::$request[$name]);
-		}
-		return null;
-	}
-	
-	private static function parse($request){
-		$items=array();
-		if(!empty($request) && is_array($request)){		
-			foreach($request as $k => $v){
-				if(!is_null($v) && trim($v)!==""){
-					$items[$k]=trim($v);
-				}				
-			}
-		}
-		return $items;
-	}
-	
-	public static function isInt($name){
-		$item=null;
-		if(isset(self::$request[$name])){
-			$item=self::$request[$name];
-			if(ctype_digit((string)$item)===true){
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public static function isDouble($name){
-		$item=null;
-		$itemExplode=array();
-		if(isset(self::$request[$name])){
-			$item=str_replace(',','.',self::$request[$name]);
-			$itemExplode=explode('.',$item);
-
-			if(count($itemExplode)==1){
-				return false;
-			}
-			
-			for($i=0; $i<count($itemExplode); $i++){
-				if(ctype_digit((string)$itemExplode[$i])===false){
-					return false;
-				}
-			}
-			
-			return true;
-		}
-		return false;
-	}
-	
-	public static function isString($name){
-		$item=null;
-		if(isset(self::$request[$name])){
-			$item=self::$request[$name];
-
-			if(is_numeric($item)){
-				return false;
-			}elseif(self::isInt($name)===true || self::isDouble($name)===true){
-				return false;				
-			}
-			return true;
-		}
-		return false;
-	}
-	
-	public static function exists($name, $type){
+    
+    private static $request=array();
+    
+    public static function setRequest($request){
+        if(!empty($request) && is_array($request)){
+            self::$request=self::parse($request);
+        }else{
+            self::$request=array();
+        }
+    }
+    
+    public static function getRequest(){
+        return self::$request;
+    }
+    
+    public static function query($name, $params=array()){
+        $item=null;
+        $allowZero=false;
+        if(isset($params['allowzero'])){
+            $allowZero=$params['allowzero'];
+        }
+        if(isset(self::$request[$name])){
+            $item=trim(self::$request[$name]);
+            
+            if($allowZero===false){
+                if($item=="0"){
+                    return null;
+                }
+            }
+            return trim(self::$request[$name]);
+        }
+        return null;
+    }
+    
+    private static function parse($request){
+        $items=array();
+        if(!empty($request) && is_array($request)){
+            foreach($request as $k => $v){
+                if(!is_null($v) && trim($v)!==""){
+                    $items[$k]=trim($v);
+                }
+            }
+        }
+        return $items;
+    }
+    
+    public static function isInt($name){
+        $item=null;
+        if(isset(self::$request[$name])){
+            $item=self::$request[$name];
+            if(ctype_digit((string)$item)===true){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static function isDouble($name){
+        $item=null;
+        $itemExplode=array();
+        
+        if(isset(self::$request[$name])){
+            $item=str_replace(',','.',self::$request[$name]);
+            $itemExplode=explode('.',$item);
+            
+            if(count($itemExplode)==1){
+                return false;
+            }
+            
+            for($i=0; $i<count($itemExplode); $i++){
+                if(ctype_digit((string)$itemExplode[$i])===false){
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+        return false;
+    }
+    
+    public static function isString($name){
+        $item=null;
+        if(isset(self::$request[$name])){
+            $item=self::$request[$name];
+            
+            if(is_numeric($item)){
+                return false;
+            }elseif(self::isInt($name)===true || self::isDouble($name)===true){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+    
+    public static function exists($name, $type){
         $item=null;
         if(isset(self::$request[$name])){
             if(!is_null($type)){
@@ -116,11 +118,19 @@ class Request{
                         return ctype_alnum((string)$name);
                     case 'alpha':
                         return ctype_alpha((string)$name);
-                }       
+                    case 'string':
+                        return (string)$name;
+                }
                 return false;
             }
             return true;
         }
         return false;
+    }
+    
+    public static function setValue($name, $value){
+        if(isset(self::$request[$name])){
+            self::$request[$name]=$value;
+        }
     }
 }
--- include/classes/Hi/Utils.php
+++ include/classes/Hi/Utils.php
@@ -1,127 +1,150 @@
 <?php
 /*
 * Created by Kakhaber Kashmadze
-@version 0.2.3
+@version 0.3
 @license MIT
 */
 
 namespace Hi;
 
 class Utils{
-	/* Parameters to use in functions */
-	private static $params=array(
-		'htmlspecialchars_decode'=>true,
-		'stripslashes'=>true,
-		'addslashes'=>true,
-		'htmlspecialchars'=>true
-	);
-	
-	/* Set or change value of one or more parameters */
-	public static function setParams($params){
-		if(!empty($params) && is_array($params) && count($params)!=0){
-			foreach($params as $key => $value){
-				self::$params[$key]=$value;
-			}
-		}
-	}
-   /* Set or change value of one parameter */
-   public static function setParam($key, $value){
-		if(!empty($key)) self::$params[$key]=$value;
-   }
 
-   /* Decode string by which encoded by function encode  */
-   public static function decode($str){
+    /* Parameters to use in functions */
+    private static $params=array(
+        'htmlspecialchars_decode'=>true,
+        'stripslashes'=>true,
+        'addslashes'=>true,
+        'htmlspecialchars'=>true
+    );
+    
+    /* Set or change value of one or more parameters */
+    public static function setParams($params){
+        if(!empty($params) && is_array($params) && count($params)!=0){
+            foreach($params as $key => $value){
+                self::$params[$key]=$value;
+            }
+        }
+    }
+    /* Set or change value of one parameter */
+    public static function setParam($key, $value){
+        if(!empty($key)) self::$params[$key]=$value;
+    }
+    
+    /* Decode string by which encoded by function encode  */
+    public static function decode($str){
         if(!empty($str)){
             if(self::$params['stripslashes']===true) $str=stripslashes($str);
-			if(self::$params['htmlspecialchars_decode']===true)	$str = htmlspecialchars_decode($str);
+            if(self::$params['htmlspecialchars_decode']===true)	$str = htmlspecialchars_decode($str);
             
             return trim($str);
         }
         return null;
     }
-
-   /* Encode string */
-   public static function encode($str) {
-
+    
+    /* Encode string */
+    public static function encode($str) {
+        
         if(!empty($str)) {
             
-			if(self::$params['htmlspecialchars']===true) $str = htmlspecialchars($str);
-			if(self::$params['addslashes']===true) $str=addslashes($str);
-				
+            if(self::$params['htmlspecialchars']===true) $str = htmlspecialchars($str);
+            if(self::$params['addslashes']===true) $str=addslashes($str);
+            
             return trim($str);
         }
         return NULL;
     }
     
-   /* Parse value and return custom or default value */
-   public static function parseValue($value, $options=array()){
-    
-		$valueType='string';
-		$defaultValue=null;
-		if(!is_array($options)) $options=(array) $options;
-		
-		if(!is_null($value)) $value=trim($value);
-		
-		if(!empty($options['type'])) $type=$options['type'];
-		if(!empty($options['defaultValue'])) $defaultValue=$options['defaultValue'];
-		if(!empty($options['nozero']) && $options['nozero']===true && $value==='0'){
-			return $defaultValue;					
-		}
-		
-		if($type=='string'){
-			if($value !== ""){				
-				if(self::$params['htmlspecialchars']===true) $value=htmlspecialchars($value);
-				if(self::$params['addslashes']===true) $value=addslashes($value);
-				return $value;
-			}
-		}elseif($type=='int'){			
-			if(trim($value) !== "" && $value !=0) return $value;
-		}elseif($type=='double'){			
-			if(trim($value)!=="" && doubleval($value)!=0.00) return $value;
-		}
-		
-		return $defaultValue;
+    /* Parse value and return custom or default value */
+    public static function parseValue($value, $options=array()){
+        
+        $valueType='string';
+        $defaultValue=null;
+        
+        if(!is_array($options)) $options=(array) $options;
+        
+        if(isset($value)){
+            $value=trim($value);
+            if(empty($value)){
+                $value=null;
+            }
+        }
+        
+        if(isset($options['type'])){
+            $valueType=$options['type'];
+        }
+        
+        if(isset($options['defaultValue'])){
+            $defaultValue=$options['defaultValue'];
+        }
+        
+        if(isset($options['nozero']) && $options['nozero']===true && ($value==='0' || $value==='0.00')){
+            return $defaultValue;
+        }
+        
+        if($valueType=='string'){
+            if(isset($value)){
+                if(self::$params['htmlspecialchars']===true){
+                    $value=htmlspecialchars($value);
+                }
+                
+                if(self::$params['addslashes']===true){
+                    $value=addslashes($value);
+                }
+                
+                return $value;
+            }
+        }elseif($valueType=='int'){            
+            if(isset($value) && ctype_digit((string)$value)){
+                return (int)$value;
+            }            
+        }elseif($valueType=='double'){
+            if(isset($value) && is_double($value)){
+                return $value;
+            }
+        }
+        
+        return $defaultValue;
     }
-
-   /* Check type of request value and return true if condition is true */
-   public static function requestType($req, $type){
-		if($type=='string'){
-			if(!empty($_REQUEST[$req])) return true;
+    
+    /* Check type of request value and return true if condition is true */
+    public static function requestType($req, $type){
+        if($type=='string'){
+            if(!empty($_REQUEST[$req])) return true;
         }elseif($type=='int'){
-			if(!empty($_REQUEST[$req]) && intval($_REQUEST[$req]) != 0) return true;
+            if(!empty($_REQUEST[$req]) && intval($_REQUEST[$req]) != 0) return true;
         }elseif($type=='double'){
-			if(!empty($_REQUEST[$req]) && doubleval(str_replace(',', '.', $_REQUEST[$req])) != 0) return true;
+            if(!empty($_REQUEST[$req]) && doubleval(str_replace(',', '.', $_REQUEST[$req])) != 0) return true;
         }
         return false;
     }
-   /* Return random string value */
-   public static function randChars($charlength){
-
+    /* Return random string value */
+    public static function randChars($charlength){
+        
         $chars = '123456789abcdfghjkmnpqrstvwxyz';
         $ret = "";
         $randStr = "";
         $i = 0;
-
+        
         while($i < $charlength){
             $randStr = mt_rand(0, strlen($chars) - 1);
             $ret .= substr($chars, $randStr, 1);
             $i++;
         }
-
+        
         return $ret;
     }
     
-   /* Check value by key in array  */
-   public static function checkInArray($check, $chckVal, $id = "id"){
+    /* Check value by key in array  */
+    public static function checkInArray($check, $chckVal, $id = "id"){
         $checkCnt = count($check);
         for($i = 0; $i < $checkCnt; $i++){
             if($check[$i][$id] == $chckVal) return 1;
         }
         return 0;
     }
-
-   /* Download file instead opening to browser: usefull when file is txt or You dont want change page when download processed */
-   public static function downloadFile($file, $fileName, $ctype) {
+    
+    /* Download file instead opening to browser: usefull when file is txt or You dont want change page when download processed */
+    public static function downloadFile($file, $fileName, $ctype) {
         if (file_exists($file)) {
             if(ob_get_level()!==0) ob_clean();
             header('Content-Description: File Transfer');
@@ -133,34 +156,34 @@ class Utils{
             exit;
         }
     }
-   
-	/* Check what request method is POST, GET ... */
-	public static function requestMethod(){
-		if(!empty($_SERVER['REQUEST_METHOD'])) return strtoupper($_SERVER['REQUEST_METHOD']);
-		return null;
-	}
-
-	/* Unlike standart empty function isEmpty also assigns true if value contains whitespaces, newlines, tabs */
-	public static function isEmpty(&$value){
-		if(empty($value) || (!is_array($value) && !is_object($value) && trim($value)=="")) return true;
-		return false;
-	}
-
-	/* alternative of var_dump with pre or json formating */
-	function vardump($var, $task=null){
-		if(!empty($task)){
-			if($task=='pre'){
-				echo '<div><pre>';
-				var_dump($var);
-				echo '</pre></div>';			
-			}elseif($task=='json'){
-				$json = json_encode((array)$var );
-				echo($json);
-			}
-		}else{
-			var_dump($var);		
-		}
-	}
+    
+    /* Check what request method is POST, GET ... */
+    public static function requestMethod(){
+        if(!empty($_SERVER['REQUEST_METHOD'])) return strtoupper($_SERVER['REQUEST_METHOD']);
+        return null;
+    }
+    
+    /* Unlike standart empty function isEmpty also assigns true if value contains whitespaces, newlines, tabs */
+    public static function isEmpty(&$value){
+        if(empty($value) || (!is_array($value) && !is_object($value) && trim($value)=="")) return true;
+        return false;
+    }
+    
+    /* alternative of var_dump with pre or json formating */
+    function vardump($var, $task=null){
+        if(!empty($task)){
+            if($task=='pre'){
+                echo '<div><pre>';
+                var_dump($var);
+                echo '</pre></div>';
+            }elseif($task=='json'){
+                $json = json_encode((array)$var );
+                echo($json);
+            }
+        }else{
+            var_dump($var);
+        }
+    }
 }
 
 ?>
